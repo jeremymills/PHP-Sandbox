f3e48d8ffc05cf210d77ced91ac13a8c1966acf6
amcgowanca <me@amcgowan.ca>
2013-03-17 11:54:12 -0400

    Began implementing some methods

--- src/PhpSandbox/Compiler/Tokenizer/Util.php
+++ src/PhpSandbox/Compiler/Tokenizer/Util.php
@@ -9,6 +9,8 @@
 
 namespace PhpSandbox\Compiler\Tokenizer;
 
+use PhpSandbox\Compiler\Tokenizer\Token\Token;
+
 /**
  * Utility class of static helper methods for tokenization.
  * 
@@ -20,20 +22,52 @@ class Util
 {
     /**
      *
-     * @return array
+     * @access public
+     * @param
+     * @return
+     */
+    public static function resolveSimpleToken($token)
+    {
+        // TODO: Complete implementation & docblock
+    }
+    
+    /**
+     * Resolves T_STRING tokens.
+     *
+     * @access public
+     * @param mixed $token The raw T_STRING token to process.
+     * @return array Returns an array of token info if valid, otherwise null.
      */
     public static function resolveTStringToken($token)
     {
+        $t = null;
         switch ($token) {
             case 'false':
+              $t = array(0 => Token::T_FALSE, 1 => 'false');
+              break;
+            
             case 'true':
+              $t = array(0 => Token::T_TRUE, 1 => 'true');
+              break;
+            
             case 'null':
+              $t = array(0 => Token::T_NULL, 1 => 'null');
+              break;
+            
             case 'self':
+              $t = array(0 => Token::T_SELF, 1 => 'self');
+              break;
+              
             case 'parent':
+              $t = array(0 => Token::T_PARENT, 1 => 'parent');
+              break;
               
             default:
+              $t = array(0 => Token::T_STRING, 1 => $token);
               break;
         }
+        
+        return $t;
     }
     
     /**
